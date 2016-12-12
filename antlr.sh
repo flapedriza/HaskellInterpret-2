689721bd3ee465c9bad217d9a06a54e7049cabab
Francesc Lapedriza <franklapedriza@gmail.com>
2016-12-12 15:42:00 +0100

    Remove unused script

--- antlr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -ne 1 ]; then
-echo "usage: $0 name_g_file"
-exit
-fi
-rm *.c *.h *.dlg
-antlr -gt $1.g
-dlg -ci parser.dlg scan.c
-g++ -o $1 $1.c scan.c err.c -I/usr/include/pccts -Wno-write-strings
